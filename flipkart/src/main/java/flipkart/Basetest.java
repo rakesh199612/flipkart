7b8f75bd1df6ae96c1b3397e32cdba754115672e
konga <konga@rakesh>
2020-08-10 18:16:53 +0530

    first

--- flipkart/src/main/java/flipkart/Basetest.java
+++ flipkart/src/main/java/flipkart/Basetest.java
@@ -5,13 +5,26 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class Basetest implements Cinstants{
 	public static WebDriver driver;
-	public void openBrowser() throws Throwable {
+	public void openBrowser(String browser) throws Throwable {
 		Basetest bt=new Basetest();
 		System.setProperty(bt.configValuse("browser_key"),bt.configValuse("browser_value"));
+		if(browser.equalsIgnoreCase("chrome"))
+		{
 		driver=new ChromeDriver();
+		}
+		if(browser.equalsIgnoreCase("firefox"))
+		{
+		driver=new FirefoxDriver();
+		}
+		if(browser.equalsIgnoreCase("opera"))
+		{
+		driver=new OperaDriver();
+		}
 	}
 	public String configValuse(String key) throws Throwable
 	{
--- flipkart/src/test/java/flipkart/Login.java
+++ flipkart/src/test/java/flipkart/Login.java
@@ -2,10 +2,12 @@ package flipkart;
 
 import org.testng.annotations.Test;
 
-public class Login {
+public class Login extends Basetest{
 	@Test
-	public void login()
+	public void login() throws Throwable
 	{
-		
+		Basetest bt=new Basetest();
+		bt.openBrowser();
+		driver.get("");
 	}
 }
